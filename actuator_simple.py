9454076eb529602a5a3db5522d53334a06583793
Juan Ramos Diaz (EXT-Nokia) <juan.ramos_diaz.ext@nokia.com>
2025-01-30 10:11:40 +0100

    better actuator with first version of comunication with lhe

--- actuator_simple.py
+++ actuator_simple.py
@@ -2,12 +2,40 @@ import threading
 import time
 import q4s_lite
 
+#Parametros q4s
 event = threading.Event()
 server_address, server_port = "127.0.0.1",20001
 client_address, client_port = "127.0.0.1",20002
 #server_address, server_port = "192.168.1.113", 20001
 #client_address, client_port = "192.168.1.50", 2000
+
+#Parametros del actuador
 actuator_alive = False
+actuator_host,actuator_port = "127.0.0.1",20003
+
+def send_command(command: str) -> str:
+    """Envía un comando al dispositivo y recibe la respuesta mediante UDP."""
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.sendto(command.encode() + b"\0", (actuator_host, actuator_port))  # Añadir byte cero al final
+        response, _ = s.recvfrom(1024)  # Recibir respuesta
+    return response.decode().strip("\0")  # Quitar byte cero al final
+
+
+def get_target_bw_orig() -> str:
+    return send_command("GET_TARGET_BW_ORIG")
+
+
+def set_target_bw_orig(value: int) -> str:
+	#Comentado en el caso que sea necesario que el numero entero explicito en big endian
+	#value_bytes = struct.pack(">I", value)  # Convierte a big-endian
+    #command = f"SET_TARGET_BW_ORIG:".encode() + value_bytes + b"\0"
+    #return send_command(command)
+    return send_command(f"SET_TARGET_BW_ORIG:{value}")
+
+
+def set_noise_resist(enable: bool) -> str:
+    return send_command(f"SET_NOISE_RESIST:{int(enable)}")
+
 
 def actuator(q4s_node):
 	#while q4s_node.running:
@@ -50,27 +78,6 @@ if __name__=="__main__":
 				q4s_node.latency_decoration=0
 			elif option == '3':
 				q4s_node.packet_loss_decoration+=0.5
-	'''actuator_thread = threading.Thread(target=actuator,args=(q4s_node,),daemon=True)
-	actuator_thread.start()
-	#El actuador no tiene que funcionar igual con el while true, podria tener un join y parar cuando quieras en la funcion actuator
-	try:
-		while True:
-			time.sleep(0.1)
-			option = input("\npulsa 0 para salir \n")
-			if option == '0':
-				actuator_alive=False
-				actuator_thread.join()
-				q4s_node.running=False
-				q4s_node.hilo_snd.join()
-				q4s_node.hilo_rcv.join()
-				
-	except KeyboardInterrupt:
-		q4s_node.running=False
-		q4s_node.hilo_snd.join()
-		q4s_node.hilo_rcv.join()
-		#actuator_thread.join() TODO cerrar bien el programa
-	print("\nYou can see q4s_client.log for viewing execution")'''
-
 
 	print("EXIT")
 	
